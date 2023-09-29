ec1df732030494d4ed3eae3885eade3806a30b33
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-09-29 11:08:39 -0300

    v

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,36 @@
-alert('Olá')
+function contar() {
+    
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados!')
+        
+        } else {
+            res.innerHTML = 'Contando... <br>'
+            var i = Number(ini.value)
+            var f = Number(fim.value)
+            var p = Number(passo.value)
+            if ( p <= 0){
+                window.alert('Passo Inválido')
+                p = 1
+            }
+            if(i < f) {
+                //contagem crescente
+                for( let c = i; c <= f; c += p) {
+                    res.innerHTML += `${c} \u{1F449}`
+               }
+        } else {
+                //contagem decrescente
+                for(let c = i; c >= f; c -= p) {
+                    res.innerHTML +=  `${c} \u{1F449}`
+               }
+         }
+         res.innerHTML += `\u{1F3C1}`
+    
+    }
+
+}
